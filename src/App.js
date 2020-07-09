bc6d806dc6779b086e2fe4f2285a2cd738268532
zhangsw1 <shuweiz314@gmail.com>
2020-07-08 20:41:26 -0700

    Setting up less loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
+import Router from './router'
+import 'antd/dist/antd.less'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu theme="dark">
+        <Menu.Item><NavLink to="/">Home</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/login">Login</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/register">Register</NavLink></Menu.Item>
+      </Menu>
+      <Router></Router>
+    </BrowserRouter>
   );
 }
 
