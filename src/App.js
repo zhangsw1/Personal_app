e457331da408379c7c44f03b2f3a7759c9e96931
zhangsw1 <shuweiz314@gmail.com>
2020-07-08 23:05:55 -0700

    moved navbar to the middle of page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom';
-import { Menu } from 'antd';
+import { BrowserRouter } from 'react-router-dom';
 import Router from './router'
-import styles from './App.module.less';
+
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Menu className={styles.menu} theme="dark">
-        <Menu.Item><NavLink to="/">Home</NavLink></Menu.Item>
-        <Menu.Item><NavLink to="/login">Login</NavLink></Menu.Item>
-        <Menu.Item><NavLink to="/register">Register</NavLink></Menu.Item>
-      </Menu>
       <Router></Router>
     </BrowserRouter>
   );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Tabs} from 'antd';
+import styles from './index.module.less'
+const { TabPane } = Tabs;
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className={styles.loginContainer}>
+            <div className={styles.login}>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab='login with Password' key='1'>
+                        addf
+                    </TabPane>
+                    <TabPane tab='login with Phone' key='w'>
+                        ad
+                    </TabPane>
+                </Tabs>
+            </div>
         </div>
     )
 }
