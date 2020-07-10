56d9c30de1e8bda67bf27b4f559a0a27dac5671a
zhangsw1 <shuweiz314@gmail.com>
2020-07-09 19:37:35 -0700

    Setting up email validation

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import InputItem from '../../components/InputItem';
+import { Form } from 'antd';
+import styles from './index.module.less'
+
 
 const Register = () => {
+    const [form] = Form.useForm();
+    const handleFinish = (values) => {
+        console.log(values)
+    }
     return (
-        <div>
-            Register
+        <div className={styles.registerContainer}>
+            <div className={styles.register}>
+                <Form
+                    form={form}
+                    onFinish={handleFinish}
+                >
+                    <InputItem
+                        name="mail"
+                        placeholder="Email"
+                        size="large"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please enter your email"
+                            },
+                            {
+                                type: 'email',
+                                message: 'Please enter valid email address'
+                            }
+                        ]}
+                    />
+                </Form>
+            </div>
         </div>
     )
 }
