6aba1329f80fbbb8318ed4edb31d317ec0917a76
zhangsw1 <shuweiz314@gmail.com>
2020-07-09 17:30:09 -0700

    Finished countingdown

--- src/components/InputItem/index.js
+++ src/components/InputItem/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import {Input, Form, Button, Row, Col} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Input, Form, Button, Row, Col, message } from 'antd';
 import styles from './index.module.less'
 
 const InputItem = (props) => {
     const { name, rules, ...rest } = props;
+    const [timing, setTiming] = useState(false); // state represent if is counting down
+    const [count, setCount] = useState(props.countDown || 60); // counting down with seconds
+    const handleClickCaptcha = () => {
+        message.success("Succefully received code 1234")
+        setTiming(true);
+    }
+
+    useEffect(()=>{
+        let interval = 0;
+        if(timing){
+            interval = window.setInterval(()=>{
+                setCount((preSecond)=>{
+                    if(preSecond <= 1){
+                        setTiming(false); // counting down finished
+                        clearInterval(interval);
+                        return props.countDown || 60;
+                    }
+                    return preSecond - 1;
+                })
+            }, 1000)
+        }
+        return () => clearInterval(interval);
+    }, [timing]);
+
     if(name === 'captcha') {
         return (
             <Form.Item name={name} rules={rules}>
@@ -12,7 +36,14 @@ const InputItem = (props) => {
                         <Input {...rest} />
                     </Col>
                     <Col span={8}>
-                        <Button className={styles.getCaptcha} size="large">Retrieve code</Button>
+                        <Button 
+                            className={styles.getCaptcha}
+                            disabled={timing} 
+                            size="large"
+                            onClick={handleClickCaptcha}
+                        >
+                        { timing ? `${count}s` : 'Retrieve code'}
+                        </Button>
                     </Col>
                     
                 </Row>
