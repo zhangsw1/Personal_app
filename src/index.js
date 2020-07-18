326b54fcb28d91e9124d33800bb96187f4ad2c36
zhangsw1 <shuweiz314@gmail.com>
2020-07-18 15:17:15 -0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { StoreContext } from 'redux-react-hook';
 import App from './App';
+import reducers from './reducers';
+
+const store = createStore (
+  reducers,
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-
-    <App />,
-
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider> 
+  ,
   document.getElementById('root')
 );
 
