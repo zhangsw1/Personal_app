fba6afbd149461a844689d19303025ff194ec680
zhangsw1 <shuweiz314@gmail.com>
2020-07-17 17:58:26 -0700

    Updated author's profile pic

--- src/pages/Home/components/ArticleListContent/index.js
+++ src/pages/Home/components/ArticleListContent/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import {Avatar} from 'antd'
 const ArticleListContent = ({
-    data: {content}
+    data: {content, avatar}
 })=>(
     <div>
         <div>{content}</div>
+        <div>
+            <Avatar src={avatar} size="small" />
+        </div>
     </div>
 )
 export default ArticleListContent;
